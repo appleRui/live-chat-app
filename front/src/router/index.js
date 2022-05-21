1c7d1dc608ef1067a3aa0ce9cfdc8867389dbefb
appleRui <apple31581@gmail.com>
2022-05-22 01:15:35 +0900

    feature/#9

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
+const routes = [{
+    path: '/login',
+    name: 'login',
+    component: () => import( /* webpackChunkName: "about" */ '@/views/auth/TheLogin.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/chatroom',
+    name: 'chatroom',
+    component: () => import( /* webpackChunkName: "about" */ '@/views/ChatRoom.vue')
+  },
 ]
 
 const router = new VueRouter({
