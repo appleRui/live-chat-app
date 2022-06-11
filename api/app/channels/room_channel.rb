e5dd60a7433e6f767012bb86ee3195061c37cf7b
appleRui <apple31581@gmail.com>
2022-06-11 09:25:16 +0900

    feature/#25

--- api/app/channels/room_channel.rb
+++ api/app/channels/room_channel.rb
@@ -1,18 +1,14 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     stream_from 'room_channel'
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-    # unsubscribed is a method that is called when a connection is disconnected. In this case, no action is taken when the connection is disconnected. We leave the empty method as it is.
+    return nil
   end
 
   def receive(data)
-    # The receive method is called each time a user types a message into the chat and the message data is received.
     user = User.find_by(email: data['email'])
-    p user.name
     if message = Message.create!(content: data['message'], user_id: user.id, room_id: data['room_id'])
       ActionCable.server.broadcast 'room_channel', { message: data['message'], name: user.name, created_at: message.created_at }
     end
